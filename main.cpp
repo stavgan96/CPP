18deee5ff1d6164846b91680b2253109b191b126
gamer16131 <gamer1love@mail.ru>
2014-09-15 22:13:06 +0400

    Revert "смотри"
    This reverts commit 8e275dfb72f3f5d9dea9a06c60aa738f9f939059.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,2 @@
-//...5
-	//Hello Azat
-
-#include <iostream>
-#include <cmath>
-using namespace std;
-void main()
-{
-int a,b,c;
-cout <<vvesti peremenuyu a<<;cin >> a>>endl;
-cout<<vvesti peremenuyu b<<;cin>>b>>endl;
-cout<<vvesti peremenuyu c<<;cin>>c>>endl;
-float z=(((b+sqrt((powb,2)+4*a*c))/2*a)-((powa,3)*c)+(powb,-2);
-cout<<peremenaya z=<<z<<endl;
-system"pause";
-}
+Test...5
+	Hello Azat
