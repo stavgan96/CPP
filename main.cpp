3cc169aab9e6ede4b00910672a504a51bd21a219
stavgan96 <shishkin96>
2014-09-15 22:12:25 +0400

    s

--- main.cpp
+++ main.cpp
@@ -1,2 +1,13 @@
-Test...5
-	Hello Azat
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void main(){
+	setlocale(LC_ALL, "Russian");
+	float a,b,c;
+	boolean job;
+	do {
+
+	} while (job);
+	system("pause");
+}
