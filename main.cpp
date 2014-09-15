1d262d473df3907edba4569a5fefad9a42a9e8eb
stavgan96 <shishkin96>
2014-09-15 22:24:07 +0400

    Азату

--- main.cpp
+++ main.cpp
@@ -1,36 +1,16 @@
-<<<<<<< HEAD
-Test...5
-	Hello Azat
-=======
-<<<<<<< HEAD
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-void main(){
-	setlocale(LC_ALL, "Russian");
-	float a,b,c;
-	boolean job;
-	do {
-
-	} while (job);
-	system("pause");
-=======
-//...5
-	//Hello Azat
-
 #include <iostream>
 #include <cmath>
 using namespace std;
 void main()
 {
 int a,b,c;
-cout <<vvesti peremenuyu a<<;cin >> a>>endl;
-cout<<vvesti peremenuyu b<<;cin>>b>>endl;
-cout<<vvesti peremenuyu c<<;cin>>c>>endl;
-float z=(((b+sqrt((powb,2)+4*a*c))/2*a)-((powa,3)*c)+(powb,-2);
-cout<<peremenaya z=<<z<<endl;
-system"pause";
->>>>>>> origin/master
+cout <<"vvesti peremenuyu a";
+cin >> a;
+cout << endl <<"vvesti peremenuyu b";
+cin>>b;
+cout << endl <<"vvesti peremenuyu c";
+cin>> c;
+float z=(((b+sqrt(pow(b,2)+4*a*c))/2*a)-((pow(a,3)*c)+(pow(b,-2))));
+cout<<"peremenaya z="<<z<<endl;
+system("pause");
 }
->>>>>>> origin/master
